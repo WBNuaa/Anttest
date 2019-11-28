adbe8a280332d846a4868e636b6d5437c664a365
bujidao1 <3218385100@qq.com>
2019-11-28 14:19:29 +0800

    Update HelloAnt.java

--- src/com/ant/HelloAnt.java
+++ src/com/ant/HelloAnt.java
@@ -7,7 +7,14 @@ package com.ant;
 public class HelloAnt {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Ant!");
+		int i = 1;
+		int sum = 0;
+		for (i=0;i<10;i++)
+		{
+			System.out.println(i);
+			sum=sum+i;
+		}
+		System.out.println(sum);
 	}
 
 }
